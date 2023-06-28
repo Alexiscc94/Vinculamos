77b77411da5c3a0ed795c145a6b7f3e415d71ba2
jorge-conejeros <jorgeconejerosrios@gmail.com>
2023-06-27 23:15:18 -0400

    Creacion de los Controllers

---
+++ aiep-Vinculamos-07/app/Http/Controllers/ParametrosController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Session;
+use App\Models\ParamCargoEncargado;
+use App\Models\ParamEstadoCompletitud;
+use App\Models\ParamEstadoEjecucion;
+use App\Models\ParamFormato;
+use App\Models\ParamImpactoExterno;
+use App\Models\ParamImpactoInterno;
+use App\Models\ParamParticipantes;
+use App\Models\ParamRecursoHumano;
+use App\Models\ParamRecursoInfraestructura;
+
+class ParametrosController extends Controller
+{
+    //
+}
---
+++ aiep-Vinculamos-07/app/Http/Controllers/ParticipationController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Session;
+use App\Models\ParticipacionPlan;
+use App\Models\ParticipacionPlanTag;
+use App\Models\ParticipacionReal;
+use App\Models\ParticipacionRealTag;
+use App\Models\IniciativasPlan;
+
+class ParticipacionController extends Controller
+{
+    //
+}
