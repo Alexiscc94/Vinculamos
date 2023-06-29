d543378070aeb56ead7ef52dbe7fcb52e7c7effa
jorge-conejeros <jorgeconejerosrios@gmail.com>
2023-06-28 22:56:58 -0400

    Avance en controladores

--- aiep-Vinculamos-07/app/Http/Controllers/ParametrosController.php
+++ aiep-Vinculamos-07/app/Http/Controllers/ParametrosController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Session;
+// Modelos
 use App\Models\ParamCargoEncargado;
 use App\Models\ParamEstadoCompletitud;
 use App\Models\ParamEstadoEjecucion;
--- aiep-Vinculamos-07/app/Http/Controllers/PlanIniciativaController.php
+++ aiep-Vinculamos-07/app/Http/Controllers/PlanIniciativaController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Session;
+// Modelos
 use App\Models\IniciativasEvidencias;
 use App\Models\IniciativasPlan;
 use App\Models\IniciativasPlanCarrera;
